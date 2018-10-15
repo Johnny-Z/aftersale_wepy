ed127c0fcb7e77e3d3e62826231e59ed0dd7add9
Johnny-Z <740602761@qq.com>
2018-10-15 16:32:09 +0800

    修改登入

--- src/api/api.js
+++ src/api/api.js
@@ -16,16 +16,17 @@ const wxJsCode2Session = (params) => wxLogin(params, apiMall + '/user/loginGetUn
 const getRepairList = (params) => wxRequest(params, apiMall + '/repair/record/selectRepairTaskByUser')
 const getMaintainList = (params) => wxRequest(params, apiMall + '/maintain/record/selectMaintainTaskByUser')
 const getInstallList = (params) => wxRequest(params, apiMall + '/install/record/selectInstallTaskByUser')
-
 const getInstallRecordList = (params) => wxRequest(params, apiMall + '/install/record/getInstallRecordInfoList')
 const getMaintainRecordList = (params) => wxRequest(params, apiMall + '/maintain/record/getMaintainRecordInfoList')
 const getRepairRecordList = (params) => wxRequest(params, apiMall + '/repair/record/getRepairRecordInfoList')
 const uploadInstallFeedback = (params) => wxRequest(params, apiMall + '/install/customer/feedback/add')
 const uploadMaintainFeedback = (params) => wxRequest(params, apiMall + '/maintain/customer/feedback/add')
 const getIssuePositionList = (params) => wxRequest(params, apiMall + '/issue/position/list/list')
+const getMobileByUser = (params) => wxRequest(params, apiMall + '/user/detail')
+const updatePassword = (params) => wxRequest(params, apiMall + '/user/updatePassword')
 
+const updateUserInfo = (params) => wxRequestRaw(params, apiMall + '/user/update')
 const uploadRepairFeedback = (params) => wxRequestRaw(params, apiMall + '/repair/actual/info/add')
-
 const updateInstallRecord = (params) => wxRequestRaw(params, apiMall + '/install/record/update')
 const updateMaintainRecord = (params) => wxRequestRaw(params, apiMall + '/maintain/record/update')
 const updateMaintainAbnormalRecord = (params) => wxRequestRaw(params, apiMall + '/maintain/abnormal/record/add')
@@ -51,5 +52,8 @@ export default {
   updateMaintainAbnormalRecord,
   uploadRepairFeedback,
   uploadRepairActualFiles,
+  getMobileByUser,
+  updateUserInfo,
+  updatePassword,
   getIssuePositionList
 }
